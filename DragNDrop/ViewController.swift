ae037d2dc753861a1f06f3c5536de9f91c47a9a2
Taha Topiwala <taha.topiwala@icloud.com>
2017-07-06 23:27:52 -0700

    Basic set up of Pan Touch Recognition - iOS 11

--- DragNDrop/ViewController.swift
+++ DragNDrop/ViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var fileViewOriginalPoint: CGPoint!
+    
+    @IBOutlet weak var file: UIImageView! {
+        didSet {
+            file.isUserInteractionEnabled = true
+            file.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(sender:))))
+            fileViewOriginalPoint = file.frame.origin
+        }
+    }
+    
+    @IBOutlet weak var trash: UIImageView! {
+        didSet {
+            trash.isUserInteractionEnabled = true
+        }
+    }
+    
+    @IBAction func resetImage(_ sender: Any) {
+        UIView.animate(withDuration: 0.5) {
+            self.file.alpha = 1
+            self.file.frame.origin = self.fileViewOriginalPoint
+        }
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.bringSubview(toFront: file)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    fileprivate func handlePan(_ view: UIView, _ sender: UIPanGestureRecognizer) {
+        
+        let transition = sender.translation(in: view)
+        
+        view.center = CGPoint(x: file.center.x + transition.x, y: file.center.y + transition.y)
+        
+        sender.setTranslation(CGPoint.zero, in: view)
     }
-
-
+    
+    fileprivate func handleEndPan(_ fileView: UIView) {
+        if fileView.frame.intersects(trash.frame) {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.file.alpha = 0
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.file.frame.origin = self.fileViewOriginalPoint
+            })
+        }
+    }
+    
+    @objc func panGesture(sender: UIPanGestureRecognizer) {
+        
+        let fileView = sender.view!
+        
+        switch sender.state {
+        case .changed, .began:
+            
+            handlePan(fileView, sender)
+            
+            break
+        case .ended:
+            
+            handleEndPan(fileView)
+            
+            break
+        default:
+            break
+        }
+    }
 }
 
